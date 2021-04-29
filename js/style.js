c9f5d29365b755640f5cd15701607624fdf17266
Anaelle693 <anaellev@icloud.com>
2021-04-29 17:09:48 +0200

    first commit

---
+++ js/style.js
@@ -0,0 +1,20 @@
+// INDEX
+$( document ).ready(function() {
+    $("header").load("header.html");
+    $("footer").load("footer.html");
+});
+
+// PAGE PRODUITS
+$( document ).ready(function() {
+    $("header").load("../header.html");
+    $("footer").load("../footer.html");
+
+    
+});
+
+// INDEX PANIER
+$( document ).ready(function() {
+    $(".header_panier").load("header_panier.html");
+    $("footer").load("footer.html");
+});
+
